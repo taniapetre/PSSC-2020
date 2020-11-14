7f0173f1d92d612be75fb3e742c91fb9017a6506
taniapetre <72751091+taniapetre@users.noreply.github.com>
2020-11-14 22:14:08 +0200

    Add files via upload

---
+++ Petre-Tania/L04/Question.Domain/CreateQuestionCmd.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    public struct CreateQuestionCmd
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Tags { get; private set; }
+
+        public CreateQuestionCmd(string Title, string Description, string Tags)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            this.Tags = Tags;
+        }
+    }
+}
---
+++ Petre-Tania/L04/Question.Domain/CreateQuestionResult.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    public static partial class CreateQuestionResult
+    {
+        public interface ICreateQuestionResult { }
+        public class QuestionCreated : ICreateQuestionResult
+        {
+            public Guid QuestionId { get; private set; }
+            public string Title { get; private set; }
+            public string Description { get; private set; }
+            public string Tags { get; private set; }
+
+            public QuestionCreated(Guid QuestionId, string Title, string Description, string Tags)
+            {
+                this.QuestionId = QuestionId;
+                this.Title = Title;
+                this.Description = Description;
+                this.Tags = Tags;
+            }
+        }
+
+        public class QuestionNotCreated : ICreateQuestionResult
+        {
+            public string Reason { get; set; }
+
+            public QuestionNotCreated(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        public class QuestionValidationFailed : ICreateQuestionResult
+        {
+            public IEnumerable<string> ValidationErrors { get; private set; }
+
+            public QuestionValidationFailed(IEnumerable<string> errors)
+            {
+                ValidationErrors = errors.AsEnumerable();
+            }
+        }
+
+    }
+}
